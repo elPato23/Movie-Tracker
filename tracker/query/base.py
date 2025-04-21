4963f3207357396f21a9bd6a44c6689015db05fe
Andres H Carrera <andreshcar@live.com>
2025-04-20 23:35:40 -0600

    feat(cli): Initial cli for TV Shows (#3)
    * Setting up things I need to run on my dev machine
    
    * Expand file to be a module instead with a folder
    
    * Spreading everything across files
    
    * Giving things super type info
    
    * Moving the API page to just be in the tvshows module so it is easier to use
    
    * First take making cli runnable
    
    * Making CLI run by defualt

---
+++ tracker/query/base.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+import requests
+
+from enum import Enum
+
+
+class TrendingTimeframe(str, Enum):
+    DAY = "day"
+    WEEK = "week"
+
+
+class TVMovieDB:
+    def __init__(self) -> None:
+        self.base_url = "https://api.themoviedb.org/3"
+        self.headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {os.getenv('MOVIEDB_API_KEY')}",
+        }
+        self.max_requests_per_second = 4
+        self.current_request_count = 0
+
+    def request(self, path: str, params: dict):
+        # Add something to cache the responses so that we can deal with them later.
+        if self.current_request_count >= 4:
+            self.current_request_count = 0
+            time.sleep(1)
+        self.current_request_count += 1
+        return requests.get(
+            f"{self.base_url}{path}", headers=self.headers, params=params
+        ).json()
