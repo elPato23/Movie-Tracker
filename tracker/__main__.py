80f63a2aba40d49f2b0b3286f7b42e20d9d8fe68
Andres Carrera <andreshcar@live.com>
2025-04-20 21:35:43 -0600

    Making CLI run by defualt

--- tracker/__main__.py
+++ tracker/__main__.py
@@ -1,14 +1,3 @@
-from tracker import TVShowAdapter, TelevisionDB
+from .cli import main
 
-
-tv_adapter = TVShowAdapter(TelevisionDB())
-trending = tv_adapter.search("How I Met your Mother")
-for show in trending:
-    print(show.name)
-    print(show.description)
-    print(
-        "Total Seasons:", show.length.seasons, "Total Episodes:", show.length.episodes
-    )
-    print(show.genres)
-    print(show.networks)
-    print("--" * 20)
+main()
