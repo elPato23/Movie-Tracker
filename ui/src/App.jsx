2a050d831eb2a98b8694983dcbe7e606180cdb39
Andres Carrera <andreshcar@live.com>
2025-04-21 16:16:27 -0600

    Adding  favicon and custom logo to the stie

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./components/pages/Home";
 import TVShow from "./components/pages/TVShow";
-
+import image from "./logo512.png";
 let navbar = [
   { name: "Home", path: "/" },
   { name: "TV Shows", path: "/tv-shows" },
@@ -12,22 +12,27 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav className="bg-gray-800 p-4 text-white">
-          <ul className="flex space-x-4">
-            {navbar.map((item, index) => {
-              return (
-                <li key={index} className="mr-4">
-                  <Link
-                    to={item.path}
-                    className="text-white hover:text-gray-300 hover:bg-gray-700 py-2 px-4 rounded"
-                  >
-                    {item.name}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
+        <header className="bg-gray-800 flex items-center justify-between p-4">
+          <Link to="/">
+            <img src={image} className="w-16 h-16" alt="Movie Tracker Logo" />
+          </Link>
+          <nav className="p-4 text-white w-full">
+            <ul className="flex space-x-4">
+              {navbar.map((item, index) => {
+                return (
+                  <li key={index} className="mr-4">
+                    <Link
+                      to={item.path}
+                      className="text-white hover:text-gray-300 hover:bg-gray-700 py-2 px-4 rounded"
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </header>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
